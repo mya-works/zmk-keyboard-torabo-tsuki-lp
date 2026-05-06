d01124bc59b5131e48123745dceaa50ac79d0229
mya-works <277258031+mya-works@users.noreply.github.com>
2026-05-06 10:45:51 +0900

    keymap-clean3

---
+++ config/JP_KP_defines.h
@@ -0,0 +1,25 @@
+// OS設定を日本語キーボードのまま使用するための変換定義
+
+#define JP_KP_DQUOTE       AT                // "
+#define JP_KP_AMPERSAND    CARET             // &
+#define JP_KP_QUOTE        AMPERSAND         // '
+#define JP_KP_EQUAL        UNDER             // =
+#define JP_KP_CARET        EQUAL             // ^
+#define JP_KP_YEN          0x89              // ¥
+#define JP_KP_PLUS         COLON             // +
+#define JP_KP_TILDE        PLUS              // ~
+#define JP_KP_PIPE         LS(0x89)          // |
+#define JP_KP_AT           LEFT_BRACKET      // @
+#define JP_KP_COLON        SINGLE_QUOTE      // :
+#define JP_KP_ASTERISK     DOUBLE_QUOTES     // *
+#define JP_KP_BACKQUOTE    LEFT_BRACE        // `
+#define JP_KP_UNDERSCORE   LS(0x87)          // _
+#define JP_KP_LBRACKET     RIGHT_BRACKET     // [
+#define JP_KP_RBRACKET     BACKSLASH         // ]
+#define JP_KP_LPAREN       ASTERISK          // (
+#define JP_KP_RPAREN       LEFT_PARENTHESIS  // )
+#define JP_KP_LBRACE       RIGHT_BRACE       // {
+#define JP_KP_RBRACE       PIPE              // }
+#define JP_KP_KANA         LANGUAGE_1        // かな
+#define JP_KP_EISU         LANGUAGE_2        // 英数
+#define JP_KP_HANZEN       GRAVE             // 半角/全角
