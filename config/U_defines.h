19be783d5a6f6d8f7c14de50f513e9f1196c717f
mya-works <277258031+mya-works@users.noreply.github.com>
2026-05-07 21:20:11 +0900

    behaviors-change7

--- config/U_defines.h
+++ config/U_defines.h
@@ -29,3 +29,6 @@
 #define JP_KP_KANA         LANGUAGE_1        // かな
 #define JP_KP_EISU         LANGUAGE_2        // 英数
 #define JP_KP_HANZEN       GRAVE             // 半角/全角
+
+#define KEYS_L 13 14 15 16 17 25 26 27 28 29 39 40 41 42 43 44 53 54 55 56 57 58
+#define KEYS_R 18 19 20 21 22 32 33 34 35 36 45 46 47 48 49 50 59 60 61 62 63 64
